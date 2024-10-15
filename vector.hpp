d47cbbcab01704398e9e53812c9784e7dfda40cd
Maxim Kozlov <maxmaz111@yandex.ru>
2024-10-15 16:50:43 +0300

    started doing something

---
+++ vector.hpp
@@ -0,0 +1,16 @@
+#include <cstddef>
+
+template <typename T> class vector {
+public:
+    vector() {
+
+    }
+
+    vector(vector&& other) {
+
+    }
+private:
+  size_t size_;
+  size_t capacity_;
+  
+};
